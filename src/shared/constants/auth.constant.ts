a53eacb76543d811984fb0fc02c1fc2096d81cb9
Tedydev <Hieudat2310.bh@gmail.com>
2025-05-05 23:48:50 +0700

    Apply Repository Pattern

--- src/shared/constants/auth.constant.ts
+++ src/shared/constants/auth.constant.ts
@@ -3,14 +3,20 @@ export const REQUEST_USER_KEY = 'user'
 export const AuthType = {
   Bearer: 'Bearer',
   None: 'None',
-  APIKey: 'ApiKey',
+  APIKey: 'ApiKey'
 } as const
 
 export type AuthTypeType = (typeof AuthType)[keyof typeof AuthType]
 
 export const ConditionGuard = {
   And: 'and',
-  Or: 'or',
+  Or: 'or'
 } as const
 
 export type ConditionGuardType = (typeof ConditionGuard)[keyof typeof ConditionGuard]
+
+export const UserStatus = {
+  ACTIVE: 'ACTIVE',
+  INACTIVE: 'INACTIVE',
+  BLOCKED: 'BLOCKED'
+} as const
