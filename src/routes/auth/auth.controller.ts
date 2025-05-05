d486401c5cc94852b9ee4752ba4de7cc3120f22a
Tedydev <Hieudat2310.bh@gmail.com>
2025-05-06 00:13:14 +0700

    Declare End Point Send OTP

---
+++ src/routes/auth/auth.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
+import { ZodSerializerDto } from 'nestjs-zod'
+import { RegisterBodyDTO, RegisterResDTO, SendOTPBodyDTO } from 'src/routes/auth/auth.dto'
+
+import { AuthService } from 'src/routes/auth/auth.service'
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  @ZodSerializerDto(RegisterResDTO)
+  async register(@Body() body: RegisterBodyDTO) {
+    return await this.authService.register(body)
+  }
+
+  @Post('otp')
+  async sendOTP(@Body() body: SendOTPBodyDTO) {
+    return await this.authService.sendOTP(body)
+  }
+
+  // @Post('login')
+  // async login(@Body() body: any) {
+  //   return this.authService.login(body)
+  // }
+
+  // @Post('refresh-token')
+  // @HttpCode(HttpStatus.OK)
+  // async refreshToken(@Body() body: any) {
+  //   return this.authService.refreshToken(body.refreshToken)
+  // }
+
+  // @Post('logout')
+  // async logout(@Body() body: any) {
+  //   return this.authService.logout(body.refreshToken)
+  // }
+}
