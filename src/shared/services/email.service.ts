cf03519452ba3cc53d8eb5e4211363925bf88897
Tedydev <Hieudat2310.bh@gmail.com>
2025-05-06 18:54:36 +0700

    #3 - Send email by Template HTML

--- src/shared/services/email.service.ts
+++ src/shared/services/email.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common'
 import { Resend } from 'resend'
 import envConfig from 'src/shared/config'
+import fs from 'fs'
+import path from 'path'
+const otpTemplate = fs.readFileSync(path.resolve('src/shared/email-templates/otp.html'), {
+  encoding: 'utf-8'
+})
 
 @Injectable()
 export class EmailService {
@@ -8,13 +13,13 @@ export class EmailService {
   constructor() {
     this.resend = new Resend(envConfig.RESEND_API_KEY)
   }
-
   sendOTP(payload: { email: string; code: string }) {
+    const subject = 'Shopsifu - Hệ thống quản trị người dùng'
     return this.resend.emails.send({
       from: 'Shopsifu Ecommerce <no-reply@demo-website.live>',
       to: [payload.email],
-      subject: 'Mã OTP của bạn là',
-      html: `<strong>${payload.code}</strong>`
+      subject,
+      html: otpTemplate.replaceAll('{{subject}}', subject).replaceAll('{{code}}', payload.code)
     })
   }
 }
