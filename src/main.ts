7c586167a931a71e25061681adeb8cc05f4540ca
Tedydev <Hieudat2310.bh@gmail.com>
2025-05-09 12:46:33 +0700

    update logger

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import helmet from 'helmet'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn']
+    logger: ['error', 'warn', 'log', 'verbose', 'debug']
   })
   app.enableCors({
     origin: '*',
