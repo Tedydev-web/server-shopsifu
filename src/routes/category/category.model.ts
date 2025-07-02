5996c4a2d171366eac25445d124ecab036acf83b
Tedydev <Hieudat2310.bh@gmail.com>
2025-07-02 16:24:25 +0700

    Refactor mô hình Brand và Category, loại bỏ các schema không cần thiết và cập nhật để sử dụng các schema từ tệp shared. Cải thiện cấu trúc mã nguồn cho các mô hình dịch thuật.

--- src/routes/category/category.model.ts
+++ src/routes/category/category.model.ts
@@ -1,23 +1,5 @@
-import { CategoryTranslationSchema } from 'src/routes/category/category-translation/category-translation.model'
+import { CategoryIncludeTranslationSchema, CategorySchema } from 'src/shared/models/shared-category.model'
 import { z } from 'zod'
-
-export const CategorySchema = z.object({
-  id: z.number(),
-  parentCategoryId: z.number().nullable(),
-  name: z.string(),
-  logo: z.string().nullable(),
-
-  createdById: z.number().nullable(),
-  updatedById: z.number().nullable(),
-  deletedById: z.number().nullable(),
-  deletedAt: z.date().nullable(),
-  createdAt: z.date(),
-  updatedAt: z.date(),
-})
-
-const CategoryIncludeTranslationSchema = CategorySchema.extend({
-  categoryTranslations: z.array(CategoryTranslationSchema),
-})
 
 export const GetAllCategoriesResSchema = z.object({
   data: z.array(CategorySchema),
@@ -45,8 +27,8 @@ export const CreateCategoryBodySchema = CategorySchema.pick({
 export const UpdateCategoryBodySchema = CreateCategoryBodySchema
 
 export type CategoryType = z.infer<typeof CategorySchema>
-export type CategoryIncludeTranslationType = z.infer<typeof CategoryIncludeTranslationSchema>
 export type GetAllCategoriesResType = z.infer<typeof GetAllCategoriesResSchema>
+export type CategoryIncludeTranslationType = z.infer<typeof CategoryIncludeTranslationSchema>
 export type GetAllCategoriesQueryType = z.infer<typeof GetAllCategoriesQuerySchema>
 export type GetCategoryDetailResType = z.infer<typeof GetCategoryDetailResSchema>
 export type CreateCategoryBodyType = z.infer<typeof CreateCategoryBodySchema>
