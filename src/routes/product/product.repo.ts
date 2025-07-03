d1975b5101a28ccc45ffed5e4b58525358d3f941
Tedydev <Hieudat2310.bh@gmail.com>
2025-07-03 15:16:47 +0700

    Khai báo method findById và delete trong ProductRepo

--- src/routes/product/product.repo.ts
+++ src/routes/product/product.repo.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common'
-import { GetProductsQueryType, GetProductsResType } from 'src/routes/product/product.model'
+import {
+  GetProductDetailResType,
+  GetProductsQueryType,
+  GetProductsResType,
+  ProductType,
+} from 'src/routes/product/product.model'
 import { ALL_LANGUAGE_CODE } from 'src/shared/constants/other.constant'
 import { PaginationService } from 'src/shared/services/pagination.service'
+import { PrismaService } from 'src/shared/services/prisma.service'
 
 @Injectable()
 export class ProductRepo {
-  constructor(private readonly paginationService: PaginationService) {}
+  constructor(
+    private readonly paginationService: PaginationService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   async list(query: GetProductsQueryType, languageId: string): Promise<GetProductsResType> {
     const { brandIds, categories, minPrice, maxPrice } = query
@@ -43,5 +52,91 @@ export class ProductRepo {
       },
       orderBy: [{ createdAt: 'desc' }],
     })
+  }
+  findById(id: number, languageId: string): Promise<GetProductDetailResType | null> {
+    return this.prismaService.product.findUnique({
+      where: {
+        id,
+        deletedAt: null,
+      },
+      include: {
+        productTranslations: {
+          where: languageId === ALL_LANGUAGE_CODE ? { deletedAt: null } : { languageId, deletedAt: null },
+        },
+        skus: {
+          where: {
+            deletedAt: null,
+          },
+        },
+        brand: {
+          include: {
+            brandTranslations: {
+              where: languageId === ALL_LANGUAGE_CODE ? { deletedAt: null } : { languageId, deletedAt: null },
+            },
+          },
+        },
+        categories: {
+          where: {
+            deletedAt: null,
+          },
+          include: {
+            categoryTranslations: {
+              where: languageId === ALL_LANGUAGE_CODE ? { deletedAt: null } : { languageId, deletedAt: null },
+            },
+          },
+        },
+      },
+    })
+  }
+
+  async delete(
+    {
+      id,
+      deletedById,
+    }: {
+      id: number
+      deletedById: number
+    },
+    isHard?: boolean,
+  ): Promise<ProductType> {
+    if (isHard) {
+      const [product] = await Promise.all([
+        this.prismaService.product.delete({
+          where: {
+            id,
+          },
+        }),
+        this.prismaService.sKU.deleteMany({
+          where: {
+            productId: id,
+          },
+        }),
+      ])
+      return product
+    }
+    const now = new Date()
+    const [product] = await Promise.all([
+      this.prismaService.product.update({
+        where: {
+          id,
+          deletedAt: null,
+        },
+        data: {
+          deletedAt: now,
+          deletedById,
+        },
+      }),
+      this.prismaService.sKU.updateMany({
+        where: {
+          productId: id,
+          deletedAt: null,
+        },
+        data: {
+          deletedAt: now,
+          deletedById,
+        },
+      }),
+    ])
+    return product
   }
 }
