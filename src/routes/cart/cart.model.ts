f53589e126f3b28d5d8b0867dc1f0f1d15dcac9f
Tedydev <Hieudat2310.bh@gmail.com>
2025-07-04 16:08:45 +0700

    Migrate Order và ProductSKUSnapshot

--- src/routes/cart/cart.model.ts
+++ src/routes/cart/cart.model.ts
@@ -1,5 +1,5 @@
-import { ProductTranslationSchema } from 'src/routes/product/product-translation/product-translation.model'
 import { ProductSchema } from 'src/shared/models/shared-product.model'
+import { ProductTranslationSchema } from 'src/shared/models/shared-product-translation.model'
 import { SKUSchema } from 'src/shared/models/shared-sku.model'
 import { UserSchema } from 'src/shared/models/shared-user.model'
 import { z } from 'zod'
