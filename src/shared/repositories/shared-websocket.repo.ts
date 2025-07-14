63fdf6b011da21489e65aa9c2d30076f3418ba55
Tedydev <Hieudat2310.bh@gmail.com>
2025-07-14 14:27:49 +0700

    Emit sự kiện về cho client khi thanh toán thành công

--- src/shared/repositories/shared-websocket.repo.ts
+++ src/shared/repositories/shared-websocket.repo.ts
@@ -5,20 +5,27 @@ import { PrismaService } from 'src/shared/services/prisma.service'
 export class SharedWebsocketRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
+  findMany(userId: number) {
+    return this.prismaService.websocket.findMany({
+      where: {
+        userId
+      }
+    })
+  }
   create(data: { id: string; userId: number }) {
     return this.prismaService.websocket.create({
       data: {
         id: data.id,
-        userId: data.userId,
-      },
+        userId: data.userId
+      }
     })
   }
 
   delete(id: string) {
     return this.prismaService.websocket.delete({
       where: {
-        id,
-      },
+        id
+      }
     })
   }
 }
