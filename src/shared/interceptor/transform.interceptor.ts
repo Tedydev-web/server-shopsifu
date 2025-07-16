1e5ed0a995574f19183ff6d11828a58fe6d1d0b2
Tedydev <Hieudat2310.bh@gmail.com>
2025-07-16 13:44:28 +0700

    Cập nhật cấu hình Docker và cải thiện logic trong TransformInterceptor để xử lý dữ liệu trả về chính xác hơn. Nâng cấp các phụ thuộc trong package.json và yarn.lock, bao gồm các phiên bản mới của @aws-sdk và react-email. Thêm quyền sở hữu cho thư mục ứng dụng trong Dockerfile.

--- src/shared/interceptor/transform.interceptor.ts
+++ src/shared/interceptor/transform.interceptor.ts
@@ -12,8 +12,13 @@ export class TransformInterceptor implements NestInterceptor {
         const statusCode = response.statusCode
         const message = response.locals?.message || 'Thành công'
         const timestamp = new Date().toISOString()
-        // Chỉ spread ra ngoài nếu có cả 'data' và 'metadata' (trường hợp pagination)
-        if (typeof data === 'object' && data !== null && 'data' in data && 'metadata' in data) {
+        // Spread ra ngoài nếu có 'data' và 'totalItems' hoặc 'metadata' (trường hợp pagination)
+        if (
+          typeof data === 'object' &&
+          data !== null &&
+          'data' in data &&
+          ('totalItems' in data || 'metadata' in data)
+        ) {
           return { statusCode, message, timestamp, ...data }
         }
         // Còn lại tất cả đều bọc vào data (object, array, primitive)
