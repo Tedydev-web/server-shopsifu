e276559ea9db499ecc9113864b8466efa55a479b
Tedydev <Hieudat2310.bh@gmail.com>
2025-07-26 15:43:54 +0700

    fix(db): Đồng bộ schema, sửa type và relation

---
+++ prisma/migrations/20250726083851_sync_database_optimization/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX "BrandTranslation_brandId_idx";
+
+-- DropIndex
+DROP INDEX "Order_createdAt_idx";
+
+-- DropIndex
+DROP INDEX "Order_userId_idx";
+
+-- DropIndex
+DROP INDEX "Review_createdAt_idx";
+
+-- DropIndex
+DROP INDEX "Review_rating_idx";
