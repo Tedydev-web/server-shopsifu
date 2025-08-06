e37c86a05ac46fb83535109145b4bcba9a7f1844
Tedydev <Hieudat2310.bh@gmail.com>
2025-08-06 12:56:29 +0700

    refactor(backend): :sparkles: cập nhật logic phân trang để hỗ trợ sắp xếp theo trường và thứ tự, cải thiện tính linh hoạt và khả năng sử dụng của API

--- src/routes/brand/brand.repo.ts
+++ src/routes/brand/brand.repo.ts
@@ -33,7 +33,7 @@ export class BrandRepo {
           }
         },
         orderBy: {
-          createdAt: 'desc'
+          [pagination.sortBy]: pagination.sortOrder
         },
         skip,
         take
--- src/shared/models/request.model.ts
+++ src/shared/models/request.model.ts
@@ -4,7 +4,9 @@ export const EmptyBodySchema = z.object({}).strict()
 
 export const PaginationQuerySchema = z.object({
   page: z.coerce.number().int().positive().default(1), // Phải thêm coerce để chuyển từ string sang number
-  limit: z.coerce.number().int().positive().default(10) // Phải thêm coerce để chuyển từ string sang number
+  limit: z.coerce.number().int().positive().default(10), // Phải thêm coerce để chuyển từ string sang number
+  sortBy: z.enum(['createdAt', 'updatedAt']).default('createdAt'),
+  sortOrder: z.enum(['asc', 'desc']).default('desc')
 })
 
 export type EmptyBodyType = z.infer<typeof EmptyBodySchema>
